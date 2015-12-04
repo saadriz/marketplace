95cabb9e72b9276edbc29e337154a8809d796aec
Saad <saad.h.rizvi@gmail.com>
2015-12-04 13:41:17 -0500

    t

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -38,10 +38,9 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
-<<<<<<< HEAD
+# <<<<<<< HEAD
 
-  }
 
 =======
->>>>>>> parent of a1f68bb... images
+# >>>>>>> parent of a1f68bb... images
 end
